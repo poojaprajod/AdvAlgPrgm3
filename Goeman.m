33f18014cae98162b25c112e0966397d46910c5c
Pooja Prajod <poojaprajod@gmail.com>
2018-01-17 23:37:55 +0100

    Fixed Decomposition and reran part1

--- Goeman.m
+++ Goeman.m
@@ -3,13 +3,16 @@ function [lower_bound,time] = Goeman(objective, A, T, n, W)
 lower_bound = inf;
 tic
 
+A = double(A) + eye(n)*0.0001;%Avoid negative values close to zero.
+[B,num] = cholcov(A);
+
 for i = 1:T
     while 1
         % generate random vector with || r || = 1
         r = randn(n,1);
         r = r/norm(r);
         %vi.r values stored in nx1 vector
-        vec = double(A*r);
+        vec = double(B'*r);
         % if vi.r >= 0, put node i in S
         S = find(vec >= 0);
         S_dash = find(vec < 0);
